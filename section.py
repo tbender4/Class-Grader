a327730464ab6a6718e470fe7927baaf00937bd4
Thomas Bender <tbender4@users.noreply.github.com>
2019-11-13 14:43:03 -0500

    Create section.py

---
+++ section.py
@@ -0,0 +1,17 @@
+class Section:
+
+    def __init__(self, sectionID):
+        self.sectionID = sectionID
+        self.sectionName
+        
+        self.attendance
+        self.homework
+        self.quiz
+        self.exam
+        
+        self.grading = []	#import the grading schema
+	    self.students = []	#array of students
+        
+#needs tkinter GUI of table layout
+#probably do text version of this database first
+
