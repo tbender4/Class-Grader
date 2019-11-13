37d03d72725b912de31219c429b12933a4fb5575
Thomas Bender <tbender4@users.noreply.github.com>
2019-11-13 14:36:22 -0500

    Update course.py

--- course.py
+++ course.py
@@ -4,6 +4,4 @@ class Course:
 	def __init__(self):
 		self.name
 		self.courseID
-		self.section
-		self.grading = []	#import the grading schema
-		self.students = []	#array of students
+		self.section = []
