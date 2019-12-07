4f0988ce77304cbd769c7f3b50da9629fc7ef32a
Thomas Bender <tbender4@gmail.com>
2019-12-07 01:19:46 -0500

    initial input of first course. course_window about to start

--- gui.py
+++ gui.py
@@ -1,15 +1,17 @@
 import tkinter
+from tkinter import simpledialog
+from course import Course
 
 class About_Modal:
   def __init__(self, window):
     description ="""
-  Class Grader is a college Python project
-  demonstrating I/O, classes, data structures, matplotlib, and tkinter.
+Class Grader is a college Python project
+demonstrating I/O, classes, data structures, matplotlib, and tkinter.
 
   Author: Thomas Bender
   Contact: tbender4@gmail.com
 
-  This software is open source and is hosted on github.com/tbender4/Class-Grader"""
+This software is open source and is hosted on github.com/tbender4/Class-Grader"""
 
     about = tkinter.Toplevel(master=window)
     about.title("Class Grader")
@@ -28,32 +30,57 @@ class About_Modal:
     about.transient(window) #makes this a temporary button
     about.grab_set()
 
+class New_Course(simpledialog.Dialog):                   #inherit tkinter.simpledialog
+  def __init__(self, parent):
+    super().__init__(parent, title="Enter Course Information:")      #inherited constructor needs original window
 
-def course_window():
+  def body(self, master):
+
+    tkinter.Label(master, text="Course ID (ex: MAC108):").grid(column = 0, row=0)
+    tkinter.Label(master, text="Course Full Name (ex: Intro to Python):").grid(column = 0, row=1)
+    self.new_course_ID = tkinter.Entry(master)
+    self.new_course_name = tkinter.Entry(master)
+
+    self.new_course_ID.grid(column=1,row=0)
+    self.new_course_name.grid(column=1,row=1)
+    
+    return None             
+
+  def validate(self):
+    if self.new_course_ID.get().strip() == '' or self.new_course_name.get().strip() == '':
+      return 0
+    return 1
+  def apply(self):
+    print("apply hit")
+    print(str(self.new_course_ID.get()))
+    self.parent.withdraw()
+    course_window(self.new_course_ID.get().strip(), self.new_course_name.get().strip())
+
+def new_course():
+  New_Course(main_window)
+
+
+def course_window(course_ID, course_name):
+  def report_size(window):
+    print(window.winfo_width(), window.winfo_height())
+
   course_window=tkinter.Toplevel()
 
+  course_window.geometry('950x800')
+  tkinter.Button(course_window, text="Report size", command=lambda: report_size(course_window)).grid(row=0)
+
+  #Menu Bar
   menu_bar = tkinter.Menu(course_window)
   file_menu = tkinter.Menu(menu_bar, tearoff=0)
   menu_bar.add_cascade(label="File", menu=file_menu)
-
   help_menu = tkinter.Menu(menu_bar, tearoff=0)
   help_menu.add_command(label="About", command=lambda: About_Modal(course_window))    #lamba fixes arguments
   menu_bar.add_cascade(label="Help", menu=help_menu)
-  
-
   course_window.config(menu=menu_bar)
-  
-  main_window.withdraw()
-  course_window.mainloop()
 
-  
+  course = Course(course_ID, course_name)
+  course.printCourse()
 
-def new_course():
-  course_init_prompt = tkinter.Tk()
-  course_init_prompt.title("Enter New Course")
-
-  courseID_label = tkinter.Label(course_init_prompt, text="Course ID:")
-  courseID_entry = tkinter.Entry(course_init_prompt)
   
 
 
@@ -63,8 +90,13 @@ main_window.title("Class Grader")
 main_window.geometry('400x300')
 
 
-new_course_button = tkinter.Button(main_window, text="New", state='normal', command=course_window)
+new_course_button = tkinter.Button(main_window, text="New", state='normal', command=new_course)
 new_course_button.grid(column=0, row=0)
 label = tkinter.Label(main_window, text='Create a new course')
 label.grid(column=1, row=0)
+
+# test_button = tkinter.Button(main_window, text="test", state='normal', command=new_course)
+# test_button.grid(column=0, row=1)
+
+
 
