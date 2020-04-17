138fdaeb801892ce8461fa3cbdef7444d9dcdf36
Thomas Bender <tbender4@gmail.com>
2020-04-17 05:50:46 -0400

    Emptied course_manager for now

--- course_manager.py
+++
@@ -1,8 +0,0 @@
-import course
-
-class Course_Manager:
-    """ Saves all courses as objects here. Separating this from GUI as a test.
-
-    Possibly will use this separation to implement Flask/alternative GUIs."""
-
-    self.course     # //Saves course here in case it's needed.
