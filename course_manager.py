4e3b765a2f47c37f42724d6ef1bc569e234a010a
Thomas Bender <tbender4@gmail.com>
2020-04-17 05:49:44 -0400

    Added preliminary json saving code
     - Incorrect counting needs to be addressed.
     - Bugs in gui still exists
     - All tangible data still not properly saved

---
+++ course_manager.py
@@ -0,0 +1,8 @@
+import course
+
+class Course_Manager:
+    """ Saves all courses as objects here. Separating this from GUI as a test.
+
+    Possibly will use this separation to implement Flask/alternative GUIs."""
+
+    self.course     # //Saves course here in case it's needed.
